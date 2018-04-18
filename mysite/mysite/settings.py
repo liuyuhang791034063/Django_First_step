62e21f67c13c9f6b2deac81fbfd78a155e9de294
liuyuhang791034063 <liuyuhang791034063@qq.com>
2018-04-18 18:17:03 +0800

    修改了模型,并且添加入数据库

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.1.1.1',]
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
